d5f73542e188d849056ef981ca380250fab4f5dc
JonathanJoelVos <dev.jonathan.joel.vos@gmail.com>
2024-12-27 16:26:53 -0300

    last

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,25 +1,25 @@
-import { CheckInRepository } from '@/repositories/check-in-repository'
-import { GymRepository } from '@/repositories/gym-repository'
-import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
-import { CheckIn } from '@prisma/client'
-import { MaxDistanceError } from './errors/max-distance-error'
-import { MaxNumberOfCheckInsError } from './errors/max-number-of-check-ins-error'
-import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { CheckInRepository } from "@/repositories/check-in-repository";
+import { GymRepository } from "@/repositories/gym-repository";
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
+import { CheckIn } from "@prisma/client";
+import { MaxDistanceError } from "./errors/max-distance-error";
+import { MaxNumberOfCheckInsError } from "./errors/max-number-of-check-ins-error";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 
 interface CheckInUseCaseRequest {
-  userId: string
-  gymId: string
-  userLongitude: number
-  userLatitude: number
+  userId: string;
+  gymId: string;
+  userLongitude: number;
+  userLatitude: number;
 }
 interface CheckInUseCaseResponse {
-  checkIn: CheckIn
+  checkIn: CheckIn;
 }
 
 export class CheckInUseCase {
   constructor(
     private checkInRepository: CheckInRepository,
-    private gymRepository: GymRepository,
+    private gymRepository: GymRepository
   ) {}
 
   async execute({
@@ -28,10 +28,10 @@ export class CheckInUseCase {
     userLongitude,
     userLatitude,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
-    const gym = await this.gymRepository.findById(gymId)
+    const gym = await this.gymRepository.findById(gymId);
 
     if (!gym) {
-      throw new ResourceNotFoundError()
+      throw new ResourceNotFoundError();
     }
 
     const distance = getDistanceBetweenCoordinates(
@@ -42,31 +42,31 @@ export class CheckInUseCase {
       {
         latitude: gym.latitude.toNumber(),
         longitude: gym.longitude.toNumber(),
-      },
-    )
+      }
+    );
 
-    const MIN_DISTANCE_IN_KM = 0.1
+    const MIN_DISTANCE_IN_KM = 0.1;
 
     if (distance > MIN_DISTANCE_IN_KM) {
-      throw new MaxDistanceError()
+      throw new MaxDistanceError();
     }
 
     const checkOnSameDate = await this.checkInRepository.findByUserIdOnDate(
       userId,
-      new Date(),
-    )
+      new Date()
+    );
 
     if (checkOnSameDate) {
-      throw new MaxNumberOfCheckInsError()
+      throw new MaxNumberOfCheckInsError();
     }
 
     const checkIn = await this.checkInRepository.create({
       gym_id: gymId,
       user_id: userId,
-    })
+    });
 
     return {
       checkIn,
-    }
+    };
   }
 }
